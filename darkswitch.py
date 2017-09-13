a2fda96c13e3c4b28e14ca574497994326af9710
James Anderson <jrsa@jrsa.co>
2017-09-13 14:15:46 -0700

    dont set plaintasks scheme and dont set sublime's theme, adaptive theme now handles that (both of these should be added back in an optional way)
    also added commands to set the current color scheme as the dark or light color scheme in the plugin settings

--- darkswitch.py
+++ darkswitch.py
@@ -2,7 +2,6 @@ import sublime, sublime_plugin
 
 PLUGIN_SETTINGS_FN = "darkswitch.sublime-settings"
 USER_SETTINGS_FN = "Preferences.sublime-settings"
-PLAINTASKS_SETTINGS_FN = "PlainTasks.sublime-settings"
 
 def plugin_settings():
   return sublime.load_settings(PLUGIN_SETTINGS_FN)
@@ -10,32 +9,37 @@ def plugin_settings():
 def user_settings():
   return sublime.load_settings(USER_SETTINGS_FN)
 
-def pt_settings():
-  return sublime.load_settings(PLAINTASKS_SETTINGS_FN)
-
 
 def flush_settings():
   sublime.save_settings(PLUGIN_SETTINGS_FN)
   sublime.save_settings(USER_SETTINGS_FN)
-  sublime.save_settings(PLAINTASKS_SETTINGS_FN)
 
 class DarkSwitchCommand(sublime_plugin.ApplicationCommand):
   def __init__(self):
     pass
 
   def run(self):
-    if(user_settings().get("theme") == plugin_settings().get("dark_theme")):
-      user_settings().set("theme", plugin_settings().get("light_theme"))
+    if(user_settings().get("color_scheme") == plugin_settings().get("dark_color_scheme")):
       user_settings().set("color_scheme", plugin_settings().get("light_color_scheme"))
-      pt_settings().set("color_scheme", plugin_settings().get("pt_light_color_scheme"))
-
-    elif(user_settings().get("theme") == plugin_settings().get("light_theme")):
-      user_settings().set("theme", plugin_settings().get("dark_theme"))
+    elif(user_settings().get("color_scheme") == plugin_settings().get("light_color_scheme")):
       user_settings().set("color_scheme", plugin_settings().get("dark_color_scheme"))
-      pt_settings().set("color_scheme", plugin_settings().get("pt_dark_color_scheme"))
-
     else:
-      print("darkswitch: plugin settings do not exist")
+      darkscheme = plugin_settings().get("dark_color_scheme")
+      user_settings().set("color_scheme", darkscheme)
 
-  flush_settings()
 
+class SetDarkCommand(sublime_plugin.ApplicationCommand):
+  def __init__(self):
+    pass
+
+  def run(self):
+    current_color_scheme = user_settings().get("color_scheme")
+    plugin_settings().set("dark_color_scheme", current_color_scheme)
+
+class SetLightCommand(sublime_plugin.ApplicationCommand):
+  def __init__(self):
+    pass
+
+  def run(self):
+    current_color_scheme = user_settings().get("color_scheme")
+    plugin_settings().set("light_color_scheme", current_color_scheme)
